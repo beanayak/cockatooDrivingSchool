ff3260845519236000a2f088783eecf57a450473
Binayak Pokharel <binayak4096@gmail.com>
2025-01-20 21:44:53 +1100

    Update Header.js
    home link changed to /

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -38,7 +38,7 @@ const Header = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0 flex items-center">
-            <Link href="#header" className="flex items-center">
+            <Link href="/" className="flex items-center">
               <FontAwesomeIcon icon={faCar} className="text-primary text-3xl mr-4" />
               <span className="font-bold text-xl">Cockatoo Driving School </span>
             </Link>
@@ -47,7 +47,7 @@ const Header = () => {
           <div className="hidden lg:flex lg:items-center lg:justify-center flex-1">
             <div className="flex items-center justify-center space-x-6">
               <Link
-                href="#header"
+                href="/"
                 className="inline-block text-gray-700 hover:bg-[#f3c614] py-2 px-4 rounded-md text-sm font-medium"
               >
                 HOME
